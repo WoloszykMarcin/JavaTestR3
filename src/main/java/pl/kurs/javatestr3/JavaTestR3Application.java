09bab273ba6dd1df448101637c7e83fb7a22dd62
Marcin Woloszyk <marcinwoloszyk412@gmail.com>
2023-08-11 11:36:26 +0200

    Initial commit

---
+++ src/main/java/pl/kurs/javatestr3/JavaTestR3Application.java
@@ -0,0 +1,13 @@
+package pl.kurs.javatestr3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestR3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestR3Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kurs/javatestr3/JavaTestR3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kurs.javatestr3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestR3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
