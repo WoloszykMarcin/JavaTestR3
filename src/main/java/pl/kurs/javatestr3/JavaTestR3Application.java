e5d11d09929be7b45485b5692bb7e8cce829d9a9
Marcin Woloszyk <marcinwoloszyk412@gmail.com>
2023-08-14 15:05:04 +0200

    changed pom.xml, application.properties added

--- src/main/java/pl/kurs/javatestr3/JavaTestR3Application.java
+++ src/main/java/pl/kurs/javatestr3/JavaTestR3Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaTestR3Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(JavaTestR3Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestR3Application.class, args);
+    }
 }
